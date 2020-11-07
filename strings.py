fd1c68cbd7ab560838dc38c34cefd5923403d766
ryanbrooks99 <ryanbrooks99@gmail.com>
2020-11-07 17:28:54 -0500

    strings

---
+++ strings.py
@@ -0,0 +1,27 @@
+#Strings in python are surrounded by either single/double quotations. 
+
+
+name = 'Ryan'
+age = 40
+
+#Concatenate
+# print('Hello, my name is ' + name + ' and I am ' + str(age))
+
+
+#String Formatting
+
+#Arguments by position
+
+# print('My name is {name} and I am {age}'.format(name=name, age=age))
+
+# F Strings (avail in 3.6+)
+
+# print(f'Hello my name is {name} and I am {age}')
+
+
+#String Methods
+
+s = 'hello world'
+
+#Capitalize
+print(s.find('r'))
